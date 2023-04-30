c18d1f83852bc425922489d9215629d1ff25609b
Rafael Forato <rafaelforato5@gmail.com>
2023-04-30 00:51:55 -0300

    changed img profile

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function toggleMode() {
     const html = document.documentElement
     html.classList.toggle('light')
 
-    const img = document.querySelector('#profile img')
-    if(html.classList.contains('light')) {
-        img.setAttribute('src', './assets/Ellipse-light.png')
-    } else {
-        img.setAttribute('src', './assets/Ellipse.png')
-    }
+    // exemplo utilizando a foto do Mayk Brito
+    // const img = document.querySelector('#profile img')
+    // if(html.classList.contains('light')) {
+    //     img.setAttribute('src', './assets/Ellipse-light.png')
+    // } else {
+    //     img.setAttribute('src', './assets/Ellipse.png')
+    // }
 }
